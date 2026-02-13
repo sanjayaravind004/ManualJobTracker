b17116f54115e700d77358cc7eae8e970e702e00
sanjayaravind004@gmail.com <yourgithubemail@example.com>
2026-02-13 11:59:48 +0530

    Initial commit

---
+++ ManualJobTracker/src/main/java/com/example/ManualJobTracker/ManualJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManualJobTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualJobTrackerApplication.class, args);
+	}
+
+}
---
+++ ManualJobTracker/src/test/java/com/example/ManualJobTracker/ManualJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManualJobTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualJobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
